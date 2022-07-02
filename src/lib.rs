3f6fdbbd8ef4530fac7ebe85842dcef6d594912f
Matteo Fantozzi <matteo.fantozzi@studenti.polito.it>
2022-07-03 00:31:34 +0200

    Added proof-of-concept init() function, used by test_init().

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,19 @@
+use pcap::Device;
+
 #[cfg(test)]
 mod tests {
+    use crate::init;
+
     #[test]
-    fn it_works() {
+    fn test_init() {
+        init();
         let result = 2 + 2;
         assert_eq!(result, 4);
     }
+}
+
+fn init() -> Result<(), pcap::Error> {
+    let list = Device::list()?;
+    println!("{:?}", list.iter().map(|x|x.name.clone()).collect::<Vec<String>>());
+    Ok(())
 }
