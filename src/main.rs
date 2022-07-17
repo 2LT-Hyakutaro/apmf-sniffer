3b9d7251ee760e6c819d13ba84fd17787321e4aa
Alberto Perugini <albertoperugini.ap@gmail.com>
2022-07-17 15:24:12 +0200

    Added filter to cl arguments, display Error in error messages

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,10 @@ struct Args {
     /// Name of the device to capture on
     #[clap(short, long = "dev", value_parser)]
     dev_name: Option<String>,
+
+    /// BFP string for capture filter
+    #[clap(short, long, value_parser, default_value = "")]
+    filter: String,
 }
 
 fn main() {
@@ -24,26 +28,27 @@ fn main() {
     let mut device;
 
     match args {
-        Args{ list: true, capture: false, dev_name:None } => {
+        Args{ list: true, capture: false, dev_name:None, filter: a } if a.as_str() == "" => {
             let list = list_devices();
             if list.is_err() {
-                println!("Could not get list of available devices");
+                println!("Could not get list of available devices: Error {:?}", list.err().unwrap());
                 return;
             }
             println!("{:?}", list.unwrap());
             return;
         },
-        Args{ list: false, capture: true, dev_name:Some(dev) } => {
-            let d = init(dev.as_str(), "");
+        Args{ list: false, capture: true, dev_name:Some(dev), filter: f } => {
+            let d = init(dev.as_str(), f.as_str());
             if d.is_err() {
-                println!("Could not initialize device {}", dev);
+                println!("Could not initialize device {}: Error {:?}", dev, d.err().unwrap());
                 return;
             }
             device = d.unwrap();
             println!("Initialized device {}", dev);
 
-            if device.start().is_err() {
-                println!("Could not start capture on device {}", dev);
+            let res = device.start();
+            if res.is_err() {
+                println!("Could not start capture on device {}: Error {:?}", dev, res.err().unwrap());
                 return;
             }
             println!("Started capture on dev {}", dev)
