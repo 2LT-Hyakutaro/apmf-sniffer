eef5f0e98f353449fc51b943d12b3109cb03bdf3
Alberto Perugini <albertoperugini.ap@gmail.com>
2022-07-24 12:01:48 +0200

    modified report for new protocol information

--- src/report.rs
+++ src/report.rs
@@ -1,11 +1,26 @@
 use std::cmp::{max, min};
 use std::collections::HashMap;
-use std::fmt::{Display, Formatter};
+use std::fmt::{Debug, Display, Formatter};
 use std::net::IpAddr;
-use crate::APMFPacket;
+use crate::{APMFPacket, Port};
 
 struct Report {
     report: HashMap<ReportHeader, ReportInfo>,
+}
+
+#[derive(PartialEq, Eq, Hash)]
+struct ReportHeader {
+    src_addr: IpAddr,
+    dest_addr: IpAddr,
+    src_port: u16,
+    dest_port: u16,
+}
+struct ReportInfo {
+    start_time: u128,
+    stop_time: u128,
+    n_bytes: u32,
+    trans_protocols: Vec<&'static str>,
+    app_protocols: Vec<Port>
 }
 
 impl Report {
@@ -21,35 +36,32 @@ impl Report {
             info.start_time = min(info.start_time, p.timestamp);
             info.stop_time = max(info.stop_time, p.timestamp);
             info.n_bytes += p.n_bytes;
-            info.protocols.push(p.protocol);
+            info.trans_protocols.push(p.protocol);
+            info.app_protocols.push(p.application);
+        }
+        else {
+            self.report.insert(p_header, ReportInfo{
+                start_time: p.timestamp,
+                stop_time: p.timestamp,
+                n_bytes: p.n_bytes,
+                trans_protocols: vec![p.protocol],
+                app_protocols: vec![p.application]
+            })
         }
     }
 }
 
 impl Display for Report {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "src address\tsrc port\tdest address\tdest port\ttime first packet\ttime last packet\tprotocols transported\tnumber of bytes\n")?;
+        write!(f, "src address\tsrc port\tdest address\tdest port\ttime first packet\ttime last packet\tnumber of bytes\ttransport protocol\tapplication protocol\n")?;
         for (header, info) in self.report.iter() {
+            let app_p = info.app_protocols.iter().map(|p| format!("{:?}", p)).collect::<Vec<String>>().join(", ");
             write!(f, "{}\t{}\t{}\t{}\t", header.src_addr, header.src_port, header.dest_addr, header.dest_port)?;
-            write!(f, "{}\t{}\t{}\t{}\n", info.start_time, info.stop_time, info.protocols.join(", "), info.n_bytes)?;
+            write!(f, "{}\t{}\t{}\t{}\t{}\n", info.start_time, info.stop_time, info.n_bytes, info.trans_protocols.join(", "), app_p)?;
         }
 
         Ok(())
     }
-}
-
-#[derive(PartialEq, Eq, Hash)]
-struct ReportHeader {
-    src_addr: IpAddr,
-    dest_addr: IpAddr,
-    src_port: u16,
-    dest_port: u16,
-}
-struct ReportInfo {
-    start_time: u128,
-    stop_time: u128,
-    n_bytes: u32,
-    protocols: Vec<&'static str>
 }
 
 fn generate_report(packets: Vec<APMFPacket>) -> Report {
